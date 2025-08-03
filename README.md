024bdacce9ce123dc0b73783fe589ae247cd3a2f
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-04 02:58:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Full-Stack MERN Employee Management System named 'Employee MS' Project: A comprehensive system designed to streamline and automate employee management.
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Employee Management System.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
