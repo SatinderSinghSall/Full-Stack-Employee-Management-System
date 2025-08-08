c866f5c0ceda7430c44d2e6f9eb9f024a14ddc82
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-09 04:21:01 +0530

    Added Password toggle implementation feature in Login.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { HiExclamationCircle, HiCheckCircle } from "react-icons/hi";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { toast } from "react-toastify";
 import { useAuth } from "../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
@@ -8,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const { login } = useAuth();
@@ -87,7 +89,7 @@ const Login = () => {
         rounded-2xl p-8 w-full max-w-sm bg-white"
       >
         <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
-          Login
+          EmployeeMS Login
         </h2>
         <form onSubmit={handleSubmit}>
           {/* Email */}
@@ -109,8 +111,8 @@ const Login = () => {
             />
           </div>
 
-          {/* Password */}
-          <div className="mb-5">
+          {/* Password with Toggle */}
+          <div className="mb-5 relative">
             <label
               htmlFor="password"
               className="block text-gray-700 mb-1 font-medium"
@@ -118,14 +120,24 @@ const Login = () => {
               Password
             </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="w-full px-3 py-2 border border-gray-300 rounded-lg 
-              focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent"
+              focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent pr-10"
               placeholder="*****"
               required
             />
+            <div
+              className="absolute right-3 top-[38px] text-gray-500 cursor-pointer hover:text-teal-600 transition-colors"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? (
+                <AiOutlineEyeInvisible size={20} />
+              ) : (
+                <AiOutlineEye size={20} />
+              )}
+            </div>
           </div>
 
           {/* Remember Me + Forgot */}
