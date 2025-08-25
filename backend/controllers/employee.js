31b437a5ec382dc87241a2563eb456c6e9cfa147
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-25 19:47:05 +0530

    Edit Employee bug reported.

--- backend/controllers/employee.js
+++ backend/controllers/employee.js
@@ -147,6 +147,7 @@ const getEmployee = async (req, res) => {
 // ================================
 // Update an employee
 // ================================
+//FIXME : Loading + Data Fetching of Departments.
 const updateEmployee = async (req, res) => {
   try {
     const { id } = req.params;
